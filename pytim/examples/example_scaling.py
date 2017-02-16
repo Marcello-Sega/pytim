89c1b9561835d9c6916c533d2fb877076a658b73
Marcello Sega <marcello.sega@gmail.com>
2017-02-16 23:05:26 +0100

    scaling example done

--- pytim/examples/example_scaling.py
+++ pytim/examples/example_scaling.py
@@ -6,40 +6,53 @@ import pytim
 from pytim.datafiles import *
 import matplotlib.pyplot as plt
 
+def plot():
+    data = np.loadtxt('timings.dat')
+    size=np.array(data[:,0]) 
+    time=np.array(data[:,1])
+#    popt, pcov = curve_fit(func, size, time,method='lm')
+#    interval = np.linspace(np.min(size),np.max(size),100)
+    plt.loglog(size,time)
+#    plt.loglog(interval,func(interval,popt[0]))
+    plt.ylabel('time (s)')
+    plt.xlabel('atoms')
+    plt.savefig("scaling.png")
 
-u       = mda.Universe(WATERSMALL_GRO)
-radii=pytim_data.vdwradii(G43A1_TOP)
-u.atoms.write("test.pdb")
+try:
+    plot()
 
-times=[]
+except:
 
-for n in range(6):
-    box = u.atoms.dimensions[:]
-    interface =pytim.ITIM(u,max_layers=1,molecular=True,cluster_cut=1.5)
-    g = u.select_atoms('all')
-    nres = g.resids[-1]
-    natm = g.ids[-1]
-    print nres
-
-    pytim.lap()
-    interface.assign_layers()
-    times.append((natm,pytim.lap()))
-
-    u2 = mda.Merge(g,g)
-    u2.atoms.dimensions=box[:]
-    u2.atoms.dimensions[1]=box[1]*2
-    u2.atoms[natm:].resids += nres
-    u2.atoms[natm:].translate([box[1],0,0])
-    u2.atoms.write("test.pdb")
-
-    del interface
-    del u2
-    del u
-    u = mda.Universe("test.pdb")
-
-np.savetxt('timings.dat',times)
-plt.loglog(zip(*times)[0],zip(*times)[1])
-plt.ylabel('time (s)')
-plt.xlabel('atoms')
-plt.savefig("scaling.png")
+    u       = mda.Universe(WATERSMALL_GRO)
+    radii=pytim_data.vdwradii(G43A1_TOP)
+    u.atoms.write("test.pdb")
+    
+    times=[]
+    
+    for n in range(9):
+        box = u.atoms.dimensions[:]
+        interface =pytim.ITIM(u,max_layers=1,molecular=True)#,cluster_cut=3.5)
+        g = u.select_atoms('all')
+        nres = g.resids[-1]
+        natm = g.ids[-1]
+        print nres," molecules"
+    
+        pytim.lap()
+        interface.assign_layers()
+        times.append((natm,pytim.lap()))
+    
+        u2 = mda.Merge(g,g)
+        u2.atoms.dimensions=box[:]
+        u2.atoms.dimensions[0]=box[0]*2
+        u2.atoms[natm:].resids += nres
+        u2.atoms[natm:].translate([box[0],0,0])
+        u2.atoms.write("test.pdb")
+    
+        del interface
+        del u2
+        del u
+        u = mda.Universe("test.pdb")
+    
+    np.savetxt('timings.dat',times)
+    plot()
 
