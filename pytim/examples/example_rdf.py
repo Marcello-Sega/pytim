4603bd3ad40f3b79d25a139280d2a79d7dcb56a4
Marcello Sega <marcello.sega@gmail.com>
2017-05-08 16:26:19 +0200

    example rdf fixed

--- pytim/examples/example_rdf.py
+++ pytim/examples/example_rdf.py
@@ -4,12 +4,14 @@ import MDAnalysis as mda
 import numpy as np
 import pytim
 from pytim.datafiles import *
-from pytim.observables import *
+from pytim import observables
 
 u = mda.Universe(WATER_GRO, WATER_XTC)
 L = np.min(u.dimensions[:3])
 oxygens = u.select_atoms("name OW")
 radii = pytim_data.vdwradii(G43A1_TOP)
+
+rdf = observables.RDF2D(u, max_radius='full', nbins=120)
 
 interface = pytim.ITIM(u, alpha=2., itim_group=oxygens,
                        max_layers=4, radii_dict=radii, cluster_cut=3.5)
@@ -17,10 +19,7 @@ interface = pytim.ITIM(u, alpha=2., itim_group=oxygens,
 for ts in u.trajectory[::5]:
     print ("frame " + str(ts.frame) + " / " + str(len(u.trajectory)))
     layer = interface.layers[0,0]
-    if ts.frame == 0:
-        rdf = InterRDF2D(layer, layer, range=(0., L / 2.), nbins=120)
-    rdf.sample(ts)
+    rdf.sample(layer,layer)
 
-rdf.normalize()
 rdf.rdf[0] = 0.0
 np.savetxt('RDF.dat', np.column_stack((rdf.bins, rdf.rdf)))
