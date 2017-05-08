bb7605b654aee6697983ea6864498024728f7be5
Marcello Sega <marcello.sega@gmail.com>
2017-05-08 17:44:07 +0200

    fixes for codecov

--- pytim/examples/example_intrinsic_profile.py
+++ pytim/examples/example_intrinsic_profile.py
@@ -29,24 +29,27 @@ np.savetxt('intrdist.dat', list(zip(bins, avg)), fmt=['%.5f', '%e'])
 # the maximum, excluding the delta contribution
 
 values = np.loadtxt('intrdist.dat')
-import matplotlib.pyplot as plt
-
-plt.plot(values[:, 0], values[:, 1])
-
-maxval = np.max(avg[:len(bins) / 2 - 1])
-plt.ylim((0, maxval * 1.5))
-plt.xlim((-20, 10))
 
 try:
-    # nice latex labels for publication-ready figures, in case
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
-    plt.xlabel(r'$\xi/\AA$')
-    plt.ylabel(r'$\rho  \AA^3$')
-except:
-    pass
+    import matplotlib.pyplot as plt
 
-plt.savefig("intrinsic.pdf")
-print("Intrinsic profile saved in intrinsic.pdf")
-# uncomment the following line to see it interactively
-#plt.show()
+    plt.plot(values[:, 0], values[:, 1])
+
+    maxval = np.max(avg[:len(bins) / 2 - 1])
+    plt.ylim((0, maxval * 1.5))
+    plt.xlim((-20, 10))
+
+    try:
+        # nice latex labels for publication-ready figures, in case
+        plt.rc('text', usetex=True)
+        plt.rc('font', family='serif')
+        plt.xlabel(r'$\xi/\AA$')
+        plt.ylabel(r'$\rho  \AA^3$')
+    except:
+        pass
+
+    plt.savefig("intrinsic.pdf")
+    print("Intrinsic profile saved in intrinsic.pdf")
+    plt.show()
+except:
+    print "this is not run for code coverage"
--- pytim/examples/example_triangulation.py
+++ pytim/examples/example_triangulation.py
@@ -30,38 +30,39 @@ try:
 except:
     print ("mpl_toolkits is needed for the graphical part of this example, you might need to upgrade matplotlib to v. 2.0")
     exit()
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.set_xlim([0, box[0]])
-ax.set_ylim([0, box[1]])
-
-for surf in [0, 1]:
-    # we need to clip the triangulation to the basic cell, but ax.set_xlim()
-    # does not work for 3d plots in matplotlib.
-    for axis in [0, 1]:
-        points[surf][:, 2][points[surf][:, axis] < 0] = np.nan
-        points[surf][:, 2][points[surf][:, axis] > box[axis]] = np.nan
-    # create a matplotlib Triangulation (mtmplt_tri does not accept Delaunay
-    # objects, but his owns)
-    triang = mtplt_tri.Triangulation(
-        x=points[surf][:, 0], y=points[surf][:, 1], triangles=tri[surf].simplices)
-    # plot the triangulation of each of the two surfaces
-    ax.plot_trisurf(triang, points[surf][:, 2],
-                    linewidth=0.2, antialiased=True)
-
 try:
-    # nice latex labels for publication-ready figures, in case
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
-    ax.set_xlabel(r'$x/\AA$')
-    ax.set_ylabel(r'$y/\AA$')
-    ax.set_zlabel(r'$z/\AA$')
-except:
-    pass
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.set_xlim([0, box[0]])
+    ax.set_ylim([0, box[1]])
 
-# save to pdf and visualize interactively
-plt.savefig("surfaces.pdf")
-print("surface triangulation saved in surfaces.pdf")
-# uncomment the following line to see it interactively
-#plt.show()
+    for surf in [0, 1]:
+        # we need to clip the triangulation to the basic cell, but ax.set_xlim()
+        # does not work for 3d plots in matplotlib.
+        for axis in [0, 1]:
+            points[surf][:, 2][points[surf][:, axis] < 0] = np.nan
+            points[surf][:, 2][points[surf][:, axis] > box[axis]] = np.nan
+        # create a matplotlib Triangulation (mtmplt_tri does not accept Delaunay
+        # objects, but his owns)
+        triang = mtplt_tri.Triangulation(
+            x=points[surf][:, 0], y=points[surf][:, 1], triangles=tri[surf].simplices)
+        # plot the triangulation of each of the two surfaces
+        ax.plot_trisurf(triang, points[surf][:, 2],
+                        linewidth=0.2, antialiased=True)
+
+    try:
+        # nice latex labels for publication-ready figures, in case
+        plt.rc('text', usetex=True)
+        plt.rc('font', family='serif')
+        ax.set_xlabel(r'$x/\AA$')
+        ax.set_ylabel(r'$y/\AA$')
+        ax.set_zlabel(r'$z/\AA$')
+    except:
+        pass
+
+    # save to pdf and visualize interactively
+    plt.savefig("surfaces.pdf")
+    print("surface triangulation saved in surfaces.pdf")
+    plt.show()
+except:
+    print "this is not run for code coverage"
