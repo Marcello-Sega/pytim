fcb1433b1a8ca5af0930b15b9290997e64394c85
Marcello Sega <marcello.sega@gmail.com>
2017-07-09 10:20:11 +0200

    updated version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.0',
+    version='0.5.1',
 
     description='Python Tool for Interfacial Molecules Analysis',
     long_description=long_description,
