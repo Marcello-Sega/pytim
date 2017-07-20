63980fe9f4147756a0a47127396ca54b554f1b5e
Marcello Sega <marcello.sega@gmail.com>
2017-07-20 15:40:16 +0200

    version updated

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.1',
+    version='0.5.2',
 
     description='Python Tool for Interfacial Molecules Analysis',
     long_description=long_description,
@@ -92,7 +92,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['MDAnalysis>=0.15','PyWavelets>=0.5.2','numpy>=1.12.0',
                       'scipy>=0.18','scikit-image>=0.13.0','cython>=0.24.1',
-                      'sphinx>=1.4.3','matplotlib','pytetgen'],
+                      'sphinx>=1.4.3','matplotlib','pytetgen>=0.1.1'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
