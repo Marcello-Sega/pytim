0748cdc4552f01c73d4907ed233949ea30f3a814
Marcello Sega <marcello.sega@gmail.com>
2017-11-29 23:46:44 +0100

    Update example_micelle.py

--- pytim/examples/example_micelle.py
+++ pytim/examples/example_micelle.py
@@ -9,7 +9,7 @@ g = u.select_atoms('resname DPC')
 
 radii = pytim_data.vdwradii(G43A1_TOP)
 
-interface = pytim.GITIM(u, itim_group=g, molecular=False,
+interface = pytim.GITIM(u, group=g, molecular=False,
                         symmetry='spherical', alpha=2.5,)
 
 layer = interface.layers[0]
