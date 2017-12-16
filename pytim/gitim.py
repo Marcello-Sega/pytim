3434fbd1933988a1ce4466b4dac177dbcfe56119
Marcello Sega <marcello.sega@gmail.com>
2017-12-16 18:18:35 +0100

    several fixes and changes to the manual
    * fixed memory problems in properties and profile
    * added support for passing a reference group in mdtraj and openmm
    * manual updated

--- pytim/gitim.py
+++ pytim/gitim.py
@@ -16,19 +16,40 @@ except:
 
 class GITIM(pytim.PYTIM):
     """Identifies interfacial molecules at macroscopically flat interfaces.
-       (Sega, M.; Kantorovich, S.; Jedlovszky, P.; Jorge, M.
-        J. Chem. Phys. 138, 044110, 2013)
+       (Sega, M.; Kantorovich, S.; Jedlovszky, P.; Jorge, M.  J. Chem. Phys. 138, 044110, 2013)
 
-        :param Universe universe:      the MDAnalysis universe
+        :param Object universe:        the MDAnalysis Universe, MDTraj trajectory\
+                                       or OpenMM Simulation objects.
+        :param Object group:           An AtomGroup, or an array-like object with\
+                                       the indices of the atoms in the group.\
+                                       Will dentify the interfacial molecules from\
+                                       this group
         :param float alpha:            the probe sphere radius
-        :param AtomGroup group:        identify the interfacial molecules\
-                                       from this group
+        :param str normal:             'x','y,'z' or 'guess'  (for planar interfaces only)
+        :param bool molecular:         Switches between search of interfacial\
+                                       molecules / atoms (default: True)
+        :param int max_layers:         the number of layers to be identified
         :param dict radii_dict:        dictionary with the atomic radii of\
                                        the elements in the group.
                                        If None is supplied, the default one\
                                        (from GROMOS 43a1) will be used.
-        :param int max_layers:         the number of layers to be identified
+        :param float cluster_cut:      cutoff used for neighbors or density-based\
+                                       cluster search (default: None disables the\
+                                       cluster analysis)
+        :param float cluster_threshold_density: Number density threshold for\
+                                       the density-based cluster search. 'auto'\
+                                       determines the threshold automatically.\
+                                       Default: None uses simple neighbors cluster\
+                                       search, if cluster_cut is not None
+        :param Object extra_cluster_groups: Additional groups, to allow for mixed interfaces
+        :param bool biggest_cluster_only: tag as surface atoms/molecules only those\
+                                       in the largest cluster. Need to specify also\
+                                       a :py:obj:`cluster_cut` value.
+        :param str symmetry:           gives the code a hint about the topology of the\
+                                       interface: 'spherical' (default) or  'planar' 
+        :param bool centered:          center the  :py:obj:`group`
         :param bool info:              print additional info
+        :param bool warnings:          print warnings 
 
         Example:
 
@@ -39,8 +60,7 @@ class GITIM(pytim.PYTIM):
         >>> u       = mda.Universe(MICELLE_PDB)
         >>> g       = u.select_atoms('resname DPC')
         >>>
-        >>> interface =pytim.GITIM(u,group=g,molecular=False,\
-                symmetry='spherical',alpha=2.5)
+        >>> interface =pytim.GITIM(u,group=g,molecular=False, alpha=2.5)
         >>> layer = interface.layers[0]
         >>> interface.writepdb('gitim.pdb',centered=False)
         >>> print repr(layer)
@@ -60,8 +80,7 @@ class GITIM(pytim.PYTIM):
         >>> # it is faster to consider only oxygens.
         >>> # Hydrogen atoms are anyway within Oxygen's radius,
         >>> # in SPC* models.
-        >>> interface =pytim.GITIM(u, group=g, molecular=True, \
-                    symmetry='spherical', alpha=2.0, max_layers=2)
+        >>> interface =pytim.GITIM(u, group=g, molecular=True, alpha=2.0, max_layers=2)
         >>>
         >>> interface.writepdb('glucose_shells.pdb')
         >>> print repr(interface.layers[0]),repr(interface.layers[1])
@@ -76,19 +95,19 @@ class GITIM(pytim.PYTIM):
     def __init__(
             self,
             universe,
-            alpha=2.0,
-            symmetry='spherical',
-            normal='guess',
             group=None,
-            radii_dict=None,
+            alpha=2.0,
+            normal='guess',
+            molecular=True,
             max_layers=1,
-            biggest_cluster_only = False,
+            radii_dict=None,
             cluster_cut=None,
             cluster_threshold_density=None,
-            molecular=True,
             extra_cluster_groups=None,
-            info=False,
+            biggest_cluster_only = False,
+            symmetry='spherical',
             centered=False,
+            info=False,
             warnings=False,
             _noextrapoints=False,
             **kargs):
@@ -281,7 +300,21 @@ class GITIM(pytim.PYTIM):
     def layers(self):
         """Access the layers as numpy arrays of AtomGroups.
 
-        The object can be sliced as usual with numpy arrays.
+        The object can be sliced as usual with numpy arrays. Differently from ITIM, 
+        there are no sides. Example:
+
+        >>> import MDAnalysis as mda
+        >>> import pytim
+        >>> from pytim.datafiles import MICELLE_PDB
+        >>>
+        >>> u = mda.Universe(MICELLE_PDB)
+        >>> micelle = u.select_atoms('resname DPC')
+        >>> interface = pytim.GITIM(u, group=micelle, max_layers=3,molecular=False)
+        >>> interface.layers  #all layers
+        array([<AtomGroup with 909 atoms>, <AtomGroup with 301 atoms>,
+               <AtomGroup with 164 atoms>], dtype=object)
+        >>> interface.layers[0]  # first layer (0)
+        <AtomGroup with 909 atoms>
 
         """
         return self._layers
