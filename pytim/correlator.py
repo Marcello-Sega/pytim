08f652ded29aa63d307dee1519b4b15c18090bb2
Marcello Sega <marcello.sega@gmail.com>
2017-12-17 21:32:17 +0100

    observables refactored

---
+++ pytim/correlator.py
@@ -0,0 +1,155 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+""" Module: correlator
+    ==================
+"""
+
+import numpy as np
+from pytim import utilities
+from MDAnalysis.core.groups import Atom, AtomGroup, Residue, ResidueGroup
+
+class Correlator(object):
+    """ Computes the (self) correlation of an observable (scalar or vector)
+
+    :param Observable observable: compute the autocorrelation of this observable
+    :param bool reduced: when the observable is a vector, average over all spatial direction if reduced==True (default)
+    :param bool normalize: normalize the correlation to 1 at t=0
+    :param AtomGroup reference: if the group passed to the sample() function changes its composition along the trajectory \
+                                (such as a layer group), a reference group that includes all atoms that could appear in the \
+                                variable group must be passed, in order to provide a proper normalization. This follows the \
+                                convention in J. Phys. Chem. B 2017, 121, 5582-5594, (DOI: 10.1021/acs.jpcb.7b02220). See\
+                                the example below.
+    :param double memory_warn: if not None, print a warning once this threshold of memory (in Mb) is passed.
+
+
+    Example:
+
+    >>> import pytim
+    >>> import MDAnalysis as mda
+    >>> import numpy as np
+    >>> from pytim.datafiles import WATERSMALL_GRO
+    >>> from pytim.utilities import lap
+    >>> #  tmpdir here is specified only for travis
+    >>> import  os
+    >>> WATERSMALL_TRR = pytim.datafiles.pytim_data.fetch('WATERSMALL_LONG_TRR',tmpdir='./')
+    checking presence of a cached copy... not found. Fetching remote file... done.
+
+    >>> u = mda.Universe(WATERSMALL_GRO,WATERSMALL_TRR)
+    >>> g = u.select_atoms('name OW')
+
+    >>> velocity = pytim.observables.Velocity()
+    >>> corr = pytim.observables.Correlator(observable=velocity)
+    >>> for t in u.trajectory[1:]:
+    ...     corr.sample(g)
+    >>> vacf = corr.correlation()
+
+    This produces the following (steps of 1 fs):
+
+    .. plot::
+
+        import pytim
+        import MDAnalysis as mda
+        import numpy as np
+        from pytim.datafiles import WATERSMALL_GRO
+        from pytim.utilities import lap
+        WATERSMALL_TRR=pytim.datafiles.pytim_data.fetch('WATERSMALL_LONG_TRR')
+
+        u = mda.Universe(WATERSMALL_GRO,WATERSMALL_TRR)
+        g = u.select_atoms('name OW')
+
+        velocity = pytim.observables.Velocity()
+        corr = pytim.observables.Correlator(observable=velocity)
+        for t in u.trajectory[1:]:
+            corr.sample(g)
+
+        vacf = corr.correlation()
+
+        from matplotlib import pyplot as plt
+        plt.plot(vacf[:1000])
+        plt.plot([0]*1000)
+        plt.show()
+
+
+    In order to compute the correlation for variable groups, one should proceed
+    as follows:
+
+    >>> corr = pytim.observables.Correlator(observable=velocity,reference=g)
+    >>> # notice the molecular=False switch, in order for the
+    >>> # layer group to be made of oxygen atoms only and match
+    >>> # the reference group
+    >>> inter = pytim.ITIM(u,group=g,alpha=2.0,molecular=False)
+    >>> for t in u.trajectory[1:10]: # example only: sample the whole trajectory
+    ...     corr.sample(inter.atoms)
+    >>> os.unlink('./'+WATERSMALL_TRR) # cleanup
+    >>> layer_vacf = corr.correlation()
+
+
+    """
+
+    def __init__(self, universe=None, observable=None, reduced=True, normalize=True, reference=None, memory_warn=None):
+        pass # wrong implementation
+        name = self.__class__.__name__
+        self.observable = observable
+        self.reference = reference
+        self.timeseries = []
+        self.maskseries = []
+        self.reduced = reduced
+        self.normalize = normalize
+        self.shape = None
+        self.mem_usage = 0.0
+        self.warned = False
+        if memory_warn is None:
+            self.warned = True
+            self.memory_warn = 0.0
+        else:
+            self.memory_warn = memory_warn
+        if reference is not None:
+            if isinstance(reference, Atom):
+                # in case just a single atom has been passed,
+                # and not a group with one atom
+                self.reference = u.atoms[reference.index:reference.index + 1]
+            if not isinstance(reference, AtomGroup):
+                raise RuntimeError(
+                    name + ': reference must be eiter an Atom or an AtomGroup')
+            self.reference_obs = observable.compute(reference) * 0.0
+            if len(self.reference_obs.shape) > 2:
+                raise RuntimeError(
+                    name + ' works only with scalar and vectors')
+
+    def sample(self, group):
+        if self.reference is not None:
+            sampled = self.reference_obs.copy()
+            mask = np.isin(self.reference, group)
+            self.maskseries.append(list(mask))
+            obs = self.observable.compute(group)
+            sampled[mask] = obs
+        else:
+            sampled = self.observable.compute(group)
+
+        self.timeseries.append(list(sampled.flatten()))
+        self.mem_usage += sampled.nbytes / 1024.0 / 1024.0  # in Mb
+        if self.mem_usage > self.memory_warn and self.warned == False:
+            print "Warning: warning threshold of",
+            print self.memory_warn, "Mb exceeded"
+            self.warned = True
+
+        if self.shape == None:
+            self.shape = sampled.shape
+
+    def correlation(self):
+        corr = utilities.correlate(self.timeseries)
+
+        if self.reference is not None:
+            mask = utilities.correlate(self.maskseries)
+            if self.shape[1] > 1:
+                tmp_mask = mask.copy()
+                for i in range(1, self.shape[1]):
+                    mask = np.hstack((mask, tmp_mask))
+            corr[mask > 0] = corr[mask > 0] / mask[mask > 0]
+        if self.reduced == True:
+            corr = np.sum(corr, axis=1)
+        if self.normalize == True:
+            corr = corr / corr[0]
+        return corr
+
+
---
+++ pytim/free_volume.py
@@ -0,0 +1,129 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+""" Module: free_volume
+    ===================
+"""
+import numpy as np
+from scipy.spatial import cKDTree
+from MDAnalysis.core.groups import Atom, AtomGroup, Residue, ResidueGroup
+
+class FreeVolume(object):
+    """ Calculates the fraction of free volume in the system, or its profile.
+
+        Note that this does not fit in the usual observable class as it can not be
+        expressed as a property of particles, and needs some kind of gridding to be calculated.
+
+        :param Universe universe:  the universe
+        :param int npoints: number of Monte Carlo sampling points (default: 10x the number of atoms in the universe)
+
+        Returns:
+        :(free volume, error) tuple : A tuple with the free volume and the estimated error
+
+        Examples:
+        >>> import MDAnalysis as mda
+        >>> import numpy as np
+        >>> import pytim
+        >>> from pytim.datafiles import CCL4_WATER_GRO, _TEST_BCC_GRO
+
+        >>> u = mda.Universe(CCL4_WATER_GRO)
+        >>> inter = pytim.ITIM(u) # just to make sure all radii are set
+        >>> np.random.seed(1) # ensure reproducibility of test
+        >>> FV = pytim.observables.FreeVolume(u)
+        >>> bins, prof ,err = FV.compute_profile()
+
+        >>> free, err = FV.compute()
+        >>> print '{0:.3f}'.format(free)+' +/- {0:.3f}'.format(err)
+        0.431 +/- 0.001
+
+        >>> # strict test on bcc volume fraction
+        >>> u = mda.Universe(_TEST_BCC_GRO)
+        >>> inter = pytim.GITIM(u,radii_dict={'C':10.*np.sqrt(3.)/4.})
+        Warning, singular matrix for  [[ 10.   0.  10.]
+         [  0.   0.  10.]
+         [ 10.  10.  10.]
+         [  0.  10.  10.]]
+        >>> nsamples = int(1e5)
+        >>> FV = pytim.observables.FreeVolume(u,npoints = nsamples)
+        >>> np.random.seed(1) # ensure reproducibility of test
+        >>> free, err = FV.compute()
+        >>> np.isclose(free,1.0-0.6802,rtol=1e-3)
+        True
+        >>> np.random.seed(1) # ensure reproducibility of test
+        >>> lst, _ = FV._compute()
+        >>> np.isclose(free,1.0-len(lst)*1.0/nsamples, rtol=1e-6)
+        True
+
+    """
+
+    def __init__(self, universe, npoints=None):
+        self.u = universe
+        if npoints is None:
+            npoints =  10* len(universe.atoms)
+        self.npoints = npoints
+
+    def _compute(self,inp=None):
+        res = np.array(0)
+        _box = self.u.dimensions.copy()
+        box = _box[:3]
+        try: # older scipy versions
+            tree = cKDTree(np.random.random((self.npoints,3)) * box,boxsize=_box[:6])
+        except:
+            tree = cKDTree(np.random.random((self.npoints,3)) * box,boxsize=_box[:3])
+        if inp is None:
+            inp = self.u.atoms
+        if not isinstance(inp,AtomGroup):
+            raise RuntimeError(self.__class__.__name__+'compute needs AtomGroup as an input')
+        # np.unique here avoids counting contributions from overlapping spheres
+        radii = np.unique(inp.radii)
+
+        for radius in radii:
+            where = np.where(np.isclose(  inp.radii ,  radius ))
+            lst = [ e for l in tree.query_ball_point(inp.positions[where],radius) for e in l]
+            res = np.append(res, lst)
+        return np.unique(res),tree.data
+
+    def compute_profile(self,inp=None,nbins=30,direction = 2):
+        """ Compute a profile of the free volume fraction
+
+            :param AtomGroup inp:  compute the volume fraction of this group, None selects the complete universe
+            :param int nbins: number of bins, by default 30
+            :param int direction: direction along wich to compute the the profile, z (2) by default
+
+            :returns bins,fraction,error: the left limit of the bins, the free volume fraction in each bin, the associated std deviation
+        """
+        box = self.u.dimensions[:3].copy()
+
+        slabwidth = box[direction] / nbins
+        slabvol = self.u.trajectory.ts.volume / nbins
+
+        bins = np.arange(nbins+1) * slabwidth
+
+        histo = []
+        error = []
+        res,data = self._compute(inp)
+        for i in range(nbins):
+            condition = np.logical_and(data[:,direction]>= bins[i],data[:,direction]<bins[i+1])
+            in_slab  = np.where(condition)[0]
+            n_in_slab = np.sum(condition*1.0)
+            if n_in_slab == 0:
+                histo.append(0.0)
+                error.append(0.0)
+            else:
+                ratio = np.sum(np.isin(res, in_slab)*1.0) / n_in_slab # occupied volume
+                histo.append(1.-ratio)
+                error.append(np.sqrt(ratio*(1.-ratio)/n_in_slab))
+        return bins,np.array(histo),np.array(error)
+
+    def compute(self,inp=None):
+        """ Compute the total free volume fraction in the simulation box
+
+            :param AtomGroup inp:  compute the volume fraction of this group, None selects the complete universe
+            :param int nbins: number of bins, by default 30
+
+            :returns fraction, error: the free volume fraction and associated error
+
+        """
+        _,free, err =  self.compute_profile(inp,nbins=1)
+        return free[0],err[0]
+
+
