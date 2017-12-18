a0433a3dede88b3b2186f14c639fc176d1784861
Marcello Sega <marcello.sega@gmail.com>
2017-12-18 23:10:54 +0100

    refactoring ongoing

--- pytim/properties.py
+++ pytim/properties.py
@@ -1,6 +1,11 @@
 # -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 import MDAnalysis
+import importlib
+import numpy as np
+from pytim.utilities import atoms_maps
+from difflib import get_close_matches
+
 
 class Layers(MDAnalysis.core.topologyattrs.AtomAttr):
     """Layers for each atom"""
@@ -42,3 +47,189 @@ def _create_property(property_name, docstring=None,
     return property(fget=getter, fset=setter, doc=docstring), absprop
 
 
+def _missing_attributes(interface, universe):
+    interface._topologyattrs = importlib.import_module(
+        'MDAnalysis.core.topologyattrs'
+    )
+    guessers = MDAnalysis.topology.guessers
+    _check_missing_attribute(interface, 'names', 'Atomnames', universe.atoms,
+                             universe.atoms.ids.astype(str), universe)
+    # NOTE _check_missing_attribute() relies on radii being set to np.nan
+    # if the attribute radii is not present
+    _check_missing_attribute(interface, 'radii', 'Radii', universe.atoms,
+                             np.nan, universe)
+    _check_missing_attribute(interface, 'tempfactors', 'Tempfactors',
+                             universe.atoms, 0.0, universe)
+    _check_missing_attribute(interface, 'bfactors', 'Bfactors',
+                             universe.atoms, 0.0, universe)
+    _check_missing_attribute(interface, 'altLocs', 'AltLocs',
+                             universe.atoms, ' ', universe)
+    _check_missing_attribute(interface, 'icodes', 'ICodes',
+                             universe.residues, ' ', universe)
+    _check_missing_attribute(interface, 'occupancies', 'Occupancies',
+                             universe.atoms, 1, universe)
+    _check_missing_attribute(interface, 'elements', 'Elements',
+                             universe.atoms, 1, universe)
+    # we add here the new layer, cluster and side information
+    # they are not part of MDAnalysis.core.topologyattrs
+    if 'layers' not in dir(universe.atoms):
+        layers = np.zeros(len(universe.atoms), dtype=np.int) - 1
+        universe.add_TopologyAttr(Layers(layers))
+
+    if 'clusters' not in dir(universe.atoms):
+        clusters = np.zeros(len(universe.atoms), dtype=np.int) - 1
+        universe.add_TopologyAttr(Clusters(clusters))
+
+    if 'sides' not in dir(universe.atoms):
+        sides = np.zeros(len(universe.atoms), dtype=np.int) - 1
+        universe.add_TopologyAttr(Sides(sides))
+
+
+def _check_missing_attribute(interface, name, classname, group, value, universe):
+    """ Add an attribute, which is necessary for pytim but
+        missing from the present topology.
+
+        An example of how the code below would expand is:
+
+        if 'radii' not in dir(universe.atoms):
+            from MDAnalysis.core.topologyattrs import Radii
+            radii = np.zeros(len(universe.atoms)) * np.nan
+            universe.add_TopologyAttr(Radii(radii))
+
+         * MDAnalysis.core.topologyattrs ->  self.topologyattrs
+         * Radii -> missing_class
+         * radii -> values
+    """
+    if name not in dir(universe.atoms):
+        missing_class = getattr(interface._topologyattrs, classname)
+        if isinstance(value, np.ndarray) or isinstance(value, list):
+            if len(value) == len(group):
+                values = np.array(value)
+            else:
+                raise RuntimeError("improper array/list length")
+        else:
+            values = np.array([value] * len(group))
+        universe.add_TopologyAttr(missing_class(values))
+        if name == 'elements':
+            types = MDAnalysis.topology.guessers.guess_types(group.names)
+            # is there an inconsistency in the way 'element' is defined
+            # different modules in MDA?
+            n0 = {'number': 0}
+            # Note: the second arg in .get() is the default.
+            group.elements = np.array(
+                [atoms_maps.get(t, n0)['number'] for t in types])
+        if name == 'radii':
+            guess_radii(interface)
+
+
+def weighted_close_match(string, dictionary):
+    # increase weight of the first letter
+    # this fixes problems with atom names like CH12
+    _wdict = {}
+    _dict = dictionary
+    _str = string[0] + string[0] + string
+    for key in _dict.keys():
+        _wdict[key[0] + key[0] + key] = _dict[key]
+    m = get_close_matches(_str, _wdict.keys(), n=1, cutoff=0.1)[0]
+    return m[2:]
+
+
+def weighted_close_match(string, dictionary):
+    # increase weight of the first letter
+    # this fixes problems with atom names like CH12
+    _wdict = {}
+    _dict = dictionary
+    _str = string[0] + string[0] + string
+    for key in _dict.keys():
+        _wdict[key[0] + key[0] + key] = _dict[key]
+    m = get_close_matches(_str, _wdict.keys(), n=1, cutoff=0.1)[0]
+    return m[2:]
+
+
+def guess_radii(interface, group=None):
+    # NOTE: this code depends on the assumption that not-set radii,
+    # have the value np.nan (see _missing_attributes() ), so don't change it
+    # let's test first which information is available
+    guessed = {}
+    try:
+        interface.guessed_radii.update({})
+    except AttributeError:
+        interface.guessed_radii = {}
+
+    universe = interface.universe
+    if group is None:
+        group = interface.universe.atoms
+
+    nans = np.isnan(group.radii)
+    # if no radius is nan, no need to guess anything
+    if not (np.any(np.equal(group.radii, None)) or np.any(nans)):
+        return
+    nones = np.equal(group.radii, None)
+    group.radii[nones] = np.array([np.nan] * len(group.radii[nones]))
+    #group.radii = group.radii.astype(np.float32)
+
+    group = group[np.isnan(group.radii)]
+
+    have_masses = ('masses' in dir(group))
+
+    have_types = False
+    if 'types' in dir(group):
+        have_types = True
+        try:
+            # When atom types are str(ints), e.g. lammps ,
+            # we cannot use them to guess radii
+
+            # trying to convert strings to integers:
+            group.types.astype(int)
+            have_types = False
+        except ValueError:
+            pass
+
+    # We give precedence to atom names, then to types
+    if have_types:
+        radii = np.copy(group.radii)
+
+        _dict = interface.radii_dict
+        for aname in np.unique(group.names):
+            try:
+                matching_type = weighted_close_match(aname, _dict)
+                radii[group.names == aname] = _dict[matching_type]
+                guessed.update({aname: _dict[matching_type]})
+            except KeyError:
+                try:
+                    atype = group.types[group.names == aname][0]
+                    matching_type = weighted_close_match(atype, _dict)
+                    radii[group.types == atype] = _dict[matching_type]
+                    guessed.update({atype: _dict[matching_type]})
+                except KeyError:
+                    pass
+
+        group.radii = np.copy(radii)
+    # We fill in the remaining ones using masses information
+
+    group = group[np.isnan(group.radii)]
+
+    if have_masses:
+        radii = np.copy(group.radii)
+        masses = group.masses
+        types = group.types
+        unique_masses = np.unique(masses)
+        # Let's not consider atoms with zero mass.
+        unique_masses = unique_masses[unique_masses > 0]
+        d = atoms_maps
+        for target_mass in unique_masses:
+            atype, mass = min(d.items(), key=lambda (
+                _, entry): abs(entry['mass'] - target_mass))
+            try:
+                matching_type = get_close_matches(
+                    atype, interface.radii_dict.keys(), n=1, cutoff=0.1
+                )
+
+                radii[masses == target_mass] = interface.radii_dict[matching_type[0]]
+
+                [guessed.update({t: interface.radii_dict[matching_type[0]]})
+                 for t in types[masses == target_mass]]
+            except:
+                pass
+        group.radii = radii
+    interface.guessed_radii.update(guessed)
--- pytim/sanity_check.py
+++ pytim/sanity_check.py
@@ -2,14 +2,14 @@
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
 from distutils.version import LooseVersion
-import importlib
 import numpy as np
 import MDAnalysis
 from . import datafiles
 from . import utilities
-from pytim.properties import Layers,Clusters,Sides,_create_property
-from difflib import get_close_matches
+from pytim.properties import Layers, Clusters, Sides, _create_property
 from . import messages
+from pytim.properties import guess_radii, _missing_attributes
+
 
 class SanityCheck(object):
 
@@ -28,7 +28,7 @@ class SanityCheck(object):
         total = self.interface.universe.atoms[0:0]  # empty group
         for g in groups:
             if g is not None:
-                self.guess_radii(group=g)
+                guess_radii(self.interface, group=g)
                 radii = np.copy(g.radii)
                 total += g
         avg = round(np.average(self.interface.radii_dict.values()), 3)
@@ -37,13 +37,13 @@ class SanityCheck(object):
         radii = np.copy(total.radii)
         radii[np.isnan(radii)] = avg
         for nantype in nantypes:
-            self.guessed_radii.update({nantype: avg})
+            self.interface.guessed_radii.update({nantype: avg})
         total.radii = radii
         try:
-            if self.guessed_radii != {} and self.interface.warnings == True:
-                print "guessed radii: ", self.guessed_radii,
+            if self.interface.guessed_radii != {} and self.interface.warnings == True:
+                print "guessed radii: ", self.interface.guessed_radii,
                 print "You can override this by using, e.g.: pytim." + self.interface.__class__.__name__,
-                print "(u,radii_dict={ '" + self.guessed_radii.keys()[0] + "':1.2 , ... } )"
+                print "(u,radii_dict={ '" + self.interface.guessed_radii.keys()[0] + "':1.2 , ... } )"
         except BaseException:
             pass
 
@@ -94,178 +94,6 @@ class SanityCheck(object):
         # fallback for itim_group
         if self.interface.itim_group is None:
             self.interface.itim_group = self.interface.all_atoms
-
-    def _missing_attributes(self, universe):
-        self.topologyattrs = importlib.import_module(
-            'MDAnalysis.core.topologyattrs'
-        )
-        guessers = MDAnalysis.topology.guessers
-        self._check_missing_attribute('names', 'Atomnames', universe.atoms,
-                                      universe.atoms.ids.astype(str), universe)
-        # NOTE _check_missing_attribute() relies on radii being set to np.nan
-        # if the attribute radii is not present
-        self._check_missing_attribute('radii', 'Radii', universe.atoms,
-                                      np.nan, universe)
-        self._check_missing_attribute('tempfactors', 'Tempfactors',
-                                      universe.atoms, 0.0, universe)
-        self._check_missing_attribute('bfactors', 'Bfactors',
-                                      universe.atoms, 0.0, universe)
-        self._check_missing_attribute('altLocs', 'AltLocs',
-                                      universe.atoms, ' ', universe)
-        self._check_missing_attribute('icodes', 'ICodes',
-                                      universe.residues, ' ', universe)
-        self._check_missing_attribute('occupancies', 'Occupancies',
-                                      universe.atoms, 1, universe)
-        self._check_missing_attribute('elements', 'Elements',
-                                      universe.atoms, 1, universe)
-        # we add here the new layer, cluster and side information
-        # they are not part of MDAnalysis.core.topologyattrs
-        if 'layers' not in dir(universe.atoms):
-            layers = np.zeros(len(universe.atoms), dtype=np.int) - 1
-            universe.add_TopologyAttr(Layers(layers))
-
-        if 'clusters' not in dir(universe.atoms):
-            clusters = np.zeros(len(universe.atoms), dtype=np.int) - 1
-            universe.add_TopologyAttr(Clusters(clusters))
-
-        if 'sides' not in dir(universe.atoms):
-            sides = np.zeros(len(universe.atoms), dtype=np.int) - 1
-            universe.add_TopologyAttr(Sides(sides))
-
-    def _check_missing_attribute(self, name, classname, group, value, universe):
-        """ Add an attribute, which is necessary for pytim but
-            missing from the present topology.
-
-            An example of how the code below would expand is:
-
-            if 'radii' not in dir(universe.atoms):
-                from MDAnalysis.core.topologyattrs import Radii
-                radii = np.zeros(len(universe.atoms)) * np.nan
-                universe.add_TopologyAttr(Radii(radii))
-
-             * MDAnalysis.core.topologyattrs ->  self.topologyattrs
-             * Radii -> missing_class
-             * radii -> values
-        """
-        if name not in dir(universe.atoms):
-            missing_class = getattr(self.topologyattrs, classname)
-            if isinstance(value, np.ndarray) or isinstance(value, list):
-                if len(value) == len(group):
-                    values = np.array(value)
-                else:
-                    raise RuntimeError("improper array/list length")
-            else:
-                values = np.array([value] * len(group))
-            universe.add_TopologyAttr(missing_class(values))
-            if name == 'elements':
-                types = MDAnalysis.topology.guessers.guess_types(group.names)
-                # is there an inconsistency in the way 'element' is defined
-                # different modules in MDA?
-                n0 = {'number': 0} 
-                # Note: the second arg in .get() is the default.
-                group.elements = np.array(
-                    [utilities.atoms_maps.get(t, n0)['number'] for t in types])
-            if name == 'radii':
-                self.guess_radii()
-
-    def weighted_close_match(self, string, dictionary):
-        # increase weight of the first letter
-        # this fixes problems with atom names like CH12
-        _wdict = {}
-        _dict = dictionary
-        _str = string[0] + string[0] + string
-        for key in _dict.keys():
-            _wdict[key[0] + key[0] + key] = _dict[key]
-        m = get_close_matches(_str, _wdict.keys(), n=1, cutoff=0.1)[0]
-        return m[2:]
-
-    def guess_radii(self, group=None):
-        # NOTE: this code depends on the assumption that not-set radii,
-        # have the value np.nan (see _missing_attributes() ), so don't change it
-        # let's test first which information is available
-        guessed = {}
-        try:
-            self.guessed_radii.update({})
-        except AttributeError:
-            self.guessed_radii = {}
-
-        universe = self.interface.universe
-        if group is None:
-            group = self.interface.universe.atoms
-
-        nans = np.isnan(group.radii)
-        # if no radius is nan, no need to guess anything
-        if not (np.any(np.equal(group.radii, None)) or np.any(nans)):
-            return
-        nones = np.equal(group.radii, None)
-        group.radii[nones] = np.array([np.nan] * len(group.radii[nones]))
-        #group.radii = group.radii.astype(np.float32)
-
-        group = group[np.isnan(group.radii)]
-
-        have_masses = ('masses' in dir(group))
-
-        have_types = False
-        if 'types' in dir(group):
-            have_types = True
-            try:
-                # When atom types are str(ints), e.g. lammps ,
-                # we cannot use them to guess radii
-
-                # trying to convert strings to integers:
-                group.types.astype(int)
-                have_types = False
-            except ValueError:
-                pass
-
-        # We give precedence to atom names, then to types
-        if have_types:
-            radii = np.copy(group.radii)
-
-            _dict = self.interface.radii_dict
-            for aname in np.unique(group.names):
-                try:
-                    matching_type = self.weighted_close_match(aname, _dict)
-                    radii[group.names == aname] = _dict[matching_type]
-                    guessed.update({aname: _dict[matching_type]})
-                except:
-                    try:
-                        atype = group.types[group.names == aname][0]
-                        matching_type = self.weighted_close_match(atype, _dict)
-                        radii[group.types == atype] = _dict[matching_type]
-                        guessed.update({atype: _dict[matching_type]})
-                    except:
-                        pass
-
-            group.radii = np.copy(radii)
-        # We fill in the remaining ones using masses information
-
-        group = group[np.isnan(group.radii)]
-
-        if have_masses:
-            radii = np.copy(group.radii)
-            masses = group.masses
-            types = group.types
-            unique_masses = np.unique(masses)
-            # Let's not consider atoms with zero mass.
-            unique_masses = unique_masses[unique_masses > 0]
-            d = utilities.atoms_maps
-            for target_mass in unique_masses:
-                atype, mass = min(d.items(), key=lambda (
-                    _, entry): abs(entry['mass'] - target_mass))
-                try:
-                    matching_type = get_close_matches(
-                        atype, self.interface.radii_dict.keys(), n=1, cutoff=0.1
-                    )
-
-                    radii[masses == target_mass] = self.interface.radii_dict[matching_type[0]]
-
-                    [guessed.update({t: self.interface.radii_dict[matching_type[0]]})
-                     for t in types[masses == target_mass]]
-                except:
-                    pass
-            group.radii = radii
-        self.guessed_radii.update(guessed)
 
     def _apply_patches(self, input_obj):
 
@@ -332,7 +160,7 @@ class SanityCheck(object):
         if radii_dict is not None:
             self.interface.radii_dict = radii_dict.copy()
 
-        self._missing_attributes(self.interface.universe)
+        _missing_attributes(self.interface, self.interface.universe)
 
     def patch_radii_dict(self):
         # fix here by hand common problems with radii assignment
@@ -394,5 +222,3 @@ class SanityCheck(object):
                 print "Warning: the option biggest_cluster_only has no effect without setting cluster_cut, ignoring it"
         except:
             pass
-
-
