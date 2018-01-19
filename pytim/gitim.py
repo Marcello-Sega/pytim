Fix for the generic intrinsic profile calculation

pytim/gitim.py
@@ -12,10 +12,10 @@ import pytim
 from pytim.sanity_check import SanityCheck
 from pytim.surface import SurfaceFlatInterface
 from pytim.surface import SurfaceGenericInterface
-try:
-    from pytetgen import Delaunay
-except ImportError:
-    from scipy.spatial import Delaunay
+#try:
+#    from pytetgen import Delaunay
+#except ImportError:
+from scipy.spatial import Delaunay
 
 
 class GITIM(pytim.PYTIM):
@@ -180,11 +180,13 @@ J. Chem. Phys. 138, 044110, 2013)*
             for simplex in t.simplices
         ])]
 
-    def circumradius(self, simplex):
+    def circumradius(self, simplex,layer):
 
-        points = self.triangulation.points
-        radii = self.triangulation.radii
-
+        try:
+            points = self.triangulation[layer].points
+            radii = self.triangulation[layer].radii
+        except IndexError:
+            raise IndexError("alpha_shape called using a wrong layer")
         R = []
         r_i = points[simplex]
         rad_i = radii[simplex]
@@ -227,7 +229,7 @@ J. Chem. Phys. 138, 044110, 2013)*
             return 0.0
         return np.min(R[R >= 0])
 
-    def alpha_shape(self, alpha, group):
+    def alpha_shape(self, alpha, group, layer):
         box = self.universe.dimensions[:3]
         delta = 2.1 * self.alpha + 1e-6
         points = group.positions[:]
@@ -253,14 +255,20 @@ J. Chem. Phys. 138, 044110, 2013)*
                 vertex = np.reshape(vertex, (1, 3))
                 extrapoints = np.append(extrapoints, vertex, axis=0)
                 extraids = np.append(extraids, -1)
-        self.triangulation = Delaunay(extrapoints)
-        self.triangulation.radii = np.append(
+        if layer == 0 :
+            self.triangulation = []
+        self.triangulation.append(Delaunay(extrapoints))
+        try:
+            triangulation = self.triangulation[layer]
+        except IndexError:
+            raise IndexError("alpha_shape called using a wrong layer")
+        triangulation.radii = np.append(
             group.radii[extraids[extraids >= 0]], np.zeros(8))
 
-        prefiltered = self.triangulation.simplices  # == skip prefiltering
+        prefiltered = triangulation.simplices  # == skip prefiltering
 
         a_shape = prefiltered[np.array([
-            self.circumradius(simplex) >= self.alpha for simplex in prefiltered
+            self.circumradius(simplex,layer) >= self.alpha for simplex in prefiltered
         ])]
         _ids = np.unique(a_shape.flatten())
         # remove the indices corresponding to the 8 additional points, which
@@ -298,7 +306,7 @@ J. Chem. Phys. 138, 044110, 2013)*
 
         for layer in range(0, self.max_layers):
 
-            alpha_ids = self.alpha_shape(self.alpha, alpha_group)
+            alpha_ids = self.alpha_shape(self.alpha, alpha_group, layer)
 
             group = alpha_group[alpha_ids]
 

pytim/profile.py
@@ -208,6 +208,7 @@ class Profile(object):
         if self.interface is None:
             pos = group.positions[::, self._dir]
         else:
+            deltabin = 1+(self._nbins-1)//2
             pos = IntrinsicDistance(
                 self.interface, symmetry=self.symmetry).compute(group)
 
@@ -229,13 +230,12 @@ class Profile(object):
                     statistic='sum',
                     bins=self._nbins)
 
-
         values = self.observable.compute(group)
-
         accum, bins, _ = stats.binned_statistic(
             pos, values, range=self._range, statistic='sum', bins=self._nbins)
-
         accum[~np.isfinite(accum)] = 0.0
+        if self.interface is not None:
+            accum[deltabin] = np.inf
 
         if self.sampled_values is None:
             self.sampled_values = accum.copy()
@@ -271,11 +271,13 @@ class Profile(object):
         if self.interface is not None:
             _vol = self.sampled_rnd_values * np.average(self._totvol)
             _vol /= np.sum(self.sampled_rnd_values)
-
+            deltabin  = np.where(~np.isfinite(vals))[0]
+            vals[deltabin] = 0.0
             vals[_vol > 0] /= _vol[_vol > 0]
             vals[_vol <= 0] *= 0.0
+            vals[deltabin] = np.inf
         else:
-            vals /= np.average(self._totvol)
+            vals /=  (np.average(self._totvol) / self._nbins)
 
         vals /= self._counts
 
@@ -285,5 +287,5 @@ class Profile(object):
             range=self._range,
             statistic='mean',
             bins=nbins)
-        avg[~np.isfinite(avg)] = 0.0
+        #avg[~np.isfinite(avg)] = 0.0
         return [bins[0:-1], bins[1:], avg]

pytim/surface.py
@@ -159,25 +159,40 @@ class Surface(object):
                                                        reference_pos, box)
         return np.average(local_env, axis=1)
 
-    def _distance_generic(self, positions, symmetry):
+    def _distance_generic(self, inp, symmetry):
 
         intr = self.interface
-        pos = positions
+        pos = utilities.extract_positions(inp)
+        
         if len(pos) == 0:
             raise ValueError("empty group")
         box = intr.universe.dimensions[:3]
         tri = utilities.find_surface_triangulation(intr)
         # positions of the points in the triangulated surface
-        l1pos = intr.triangulation.points[tri]
+        l1pos = intr.triangulation[0].points[tri]
         # their baricenters
-        l1centers = np.average(l1pos, axis=1)
+        l1centers = utilities.pbc_wrap(np.average(l1pos, axis=1),box)
         # tree of the surface triangles' centers
         tree = cKDTree(l1centers, boxsize=box)
 
         # distances and indices of the surface triangles' centers to pos[]
         dist, ind = tree.query(pos, k=1)
-        # we need now the position of the nearest triangle's centers
-        # selected above.
+
+        if isinstance(inp,np.ndarray):
+            # An array of positions has been passed (e.g. for the Monte Carlo volume estimate)
+            # we need to check if they are close to any surface atom
+            # This is not necessarily exact.
+            for tr in [0,1,2]:
+                cond0 = np.all(np.isclose(l1pos[ind,tr],pos),axis=1)
+                dist[cond0] = 0.0
+        else:
+            # a group has been passed, we know exactly which atoms are surface ones.
+            try:
+                dist[inp.atoms.layers==1] = 0.0  # Warning, the corresponding values of 'ind' will be wrong.
+                                                 # If you change this code, check that it won't 
+                                                 # depend on 'ind'
+            except AttributeError:
+                raise RuntimeError("Internal error: wrong parameter passed to _distance_generic")
         l1centers = l1centers[ind]
 
         if symmetry == 'generic':
@@ -190,7 +205,10 @@ class Surface(object):
             P2 = utilities.pbc_compact(COM, l1centers, box) - l1centers
             sign = -np.sign(np.sum(P1 * P2, axis=1))
             return sign * dist
+
         raise ValueError("Incorrect symmetry used for distance calculation")
+
+
 
     def _distance_spherical(self, positions):
         box = self.interface.universe.dimensions[:3]
@@ -275,8 +293,7 @@ class SurfaceFlatInterface(Surface):
 class SurfaceGenericInterface(Surface):
     def distance(self, inp, *args):
         symmetry = args[0]
-        positions = utilities.extract_positions(inp)
-        return self._distance_generic(positions, symmetry)
+        return self._distance_generic(inp, symmetry)
 
     def interpolation(self, inp):
         pass