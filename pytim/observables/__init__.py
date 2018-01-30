005e2b295ba24322e7ac27a00c44c55b6fe8cf66
Marcello Sega <marcello.sega@gmail.com>
2018-01-30 20:10:06 +0100

    cleanup

--- pytim/observables/__init__.py
+++ pytim/observables/__init__.py
@@ -12,7 +12,8 @@ from basic_observables import Number, Mass, Charge, NumberOfResidues
 
 from intrinsic_distance import IntrinsicDistance
 from profile import Profile
-from rdf import RDF, RDF2D
+from rdf import RDF
+from rdf2d import RDF2D
 from free_volume import FreeVolume
 from correlator import Correlator
 from orientation import Orientation
