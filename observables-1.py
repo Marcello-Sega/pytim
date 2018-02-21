bbaf7b4399c4d9070849a0a50e8712f3084b8cb3
Marcello Sega <marcello.sega@gmail.com>
2018-02-21 12:22:38 +0100

    update

--- observables-1.py
+++ observables-1.py
@@ -1,19 +1,32 @@
-import MDAnalysis as mda
+from matplotlib import pyplot as plt
+
 import numpy as np
+import MDAnalysis as mda
 import pytim
-import matplotlib.pyplot as plt
 from   pytim.datafiles import *
+from   pytim.observables import Profile
+
 u = mda.Universe(WATER_GRO,WATER_XTC)
-oxygens = u.select_atoms("name OW")
-interface = pytim.ITIM(u,alpha=2.,group=oxygens,                               cluster_cut=3.5, molecular=False)
-rdf=pytim.observables.RDF2D(u,nbins=250)
-for ts in u.trajectory[::50] :
-    layer=interface.layers[0,0]
-    rdf.sample(layer,layer)
-rdf.count[0]=0
+g=u.select_atoms('name OW')
+# here we calculate the profiles of oxygens only (note molecular=False)
+inter = pytim.ITIM(u,group=g,max_layers=4,centered=True, molecular=False)
 
-plt.plot(rdf.bins, rdf.rdf)
+Layers=[]
+# by default Profile() uses the number of atoms as an observable
+for n in np.arange(0,5):
+    Layers.append(Profile())
 
-plt.gca().set_xlim([0,7])
+for ts in u.trajectory[::50]:
+    for n in range(len(Layers)):
+        if n>0:
+            group = u.atoms[u.atoms.layers == n ]
+        else:
+            group = g
+        Layers[n].sample(group)
 
+for L in Layers:
+    low,up,avg = L.get_values(binwidth=0.5)
+    plt.plot(low,avg)
+
+plt.gca().set_xlim([80,120])
 plt.show()
