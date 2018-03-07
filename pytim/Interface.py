95b8f3f2eccce848d86ff71d0d9ec4c63c66c014
Marcello Sega <marcello.sega@gmail.com>
2018-03-07 17:52:59 +0100

    added doctests for SimpleInterface

--- pytim/Interface.py
+++ pytim/Interface.py
@@ -642,6 +642,71 @@ from .surface import SurfaceFlatInterface
 from .surface import SurfaceGenericInterface
 
 class SimpleInterface(Interface):
+    """ This simple interface is designed to allow the user to define
+        its own interface by supplying one or more groups, and be able to 
+        use all other analysis tools of pytim, which require an interface,
+        also when it is not advisable/possible to use any of the other
+        classes
+    
+        :param Universe universe: The MDAnalysis_ Universe.
+        :param AtomGroup group:   The AtomGroup with the interfacial atoms.
+                                  (only for symmetry='generic'). See the upper
+                                  and lower groups options below.
+        :param float alpha:       The probe sphere radius (used for handling
+                                  the periodicity in the surface layer
+                                  triangulation only)
+        :param str normal:        The macroscopic interface normal direction
+                                  'x','y', or 'z'(default)
+        :param str symmetry:      Either 'grneric' (default) or 'planar'.
+                                  Selects the type of interpolation/distance
+                                  calculation. If 'planar' two groups have to
+                                  be also passed (upper and lower)
+        :param AtomGroup upper:   The upper surface group (if symmetry='planar')
+        :param AtomGroup lower:   The lower surface group (if symmetry='planar')
+    
+    
+        Example: compute an intrinsic profile by interpolating the surface
+        position from the P atoms of a DPPC bilayer
+    
+        >>> import pytim
+        >>> import numpy as np
+        >>> from pytim.datafiles import DPPC_GRO
+        >>> import MDAnalysis as mda
+        >>> u = mda.Universe(DPPC_GRO)
+        >>> p = u.select_atoms('name P8')
+        >>> 
+        >>> dppc  = u.select_atoms('resname DPPC')
+        >>> water = u.select_atoms('resname SOL')
+        >>> 
+        >>> box = u.dimensions[:3]
+        >>> # here we want to use as 'surface' atoms only the P atoms
+        >>> # and compute an intrinsic profile with respect to the
+        >>> # interpolated surface.
+        >>> # We need to distinguish the upper and lower leaflet by hand! 
+        >>> upper = p[p.positions[:,2]>box[2]/2.]
+        >>> lower = p - upper
+        >>> 
+        >>> # if symmetry=='planar', supply an upper and a lower group
+        >>> # if symmetry=='generic', supply only group
+        >>> 
+        >>> # here we need a large value of alpha (for pbc reconstruction 
+        >>> # of the triangulation used for the interpolation)
+        >>> inter = pytim.SimpleInterface(u,symmetry='planar', upper=upper, lower=lower,alpha=5.) 
+        >>> 
+        >>> profile1 = pytim.observables.Profile(interface=inter)
+        >>> profile2 = pytim.observables.Profile(interface=inter)
+        >>> np.random.seed(1)
+        >>> profile1.sample(dppc)
+        >>> profile2.sample(water)
+        >>> 
+        >>> lo,up,av1 = profile1.get_values(binwidth=.5)
+        >>> lo,up,av2 = profile2.get_values(binwidth=.5)
+        >>> np.set_printoptions(8)
+        >>> print (av2[64:70])
+        [0.04292889 0.04287077 0.04278019        inf 0.0447655  0.05189449]
+    
+    """
+
     def __init__(self, 
                  universe, 
                  group=None, 
@@ -665,9 +730,6 @@ class SimpleInterface(Interface):
         sanity.assign_universe(
             universe, radii_dict=None, warnings=False)
         sanity.assign_alpha(alpha)
-
-
-
         sanity.assign_radii()
         if normal in [0,1,2]:
             self.normal = normal
@@ -694,8 +756,6 @@ class SimpleInterface(Interface):
             self._surfaces[0] = SurfaceGenericInterface(
                     self, options={'layer': 0})
         
-
-            
     def _assign_layers(self):
         pass
 #
