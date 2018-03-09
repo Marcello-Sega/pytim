492a5d69e3b14b67dd0ac549c95f3838f7056b8d
Marcello Sega <marcello.sega@gmail.com>
2018-03-09 16:37:46 +0000

    Apply fixes from StyleCI

--- pytim/gaussian_kde_pbc.py
+++ pytim/gaussian_kde_pbc.py
@@ -19,19 +19,19 @@ class gaussian_kde_pbc(gaussian_kde):
         d = self.sigma * 2.5
         results = np.zeros(grid.shape[1], dtype=float)
         gridT = grid.T[:]
-        tree = cKDTree(gridT,boxsize=box)
+        tree = cKDTree(gridT, boxsize=box)
         # the indices of grid elements within distane d from each of the pos
-        scale  = 2.*self.sigma**2
-        indlist  = tree.query_ball_point(pos,d)
-        for n,ind in enumerate(indlist):
-            dr = gridT[ind,:] - pos[n]
-            cond = np.where(dr > box/2.)
-            dr [cond] -= box[cond[1]]
-            cond = np.where(dr < -box/2.)
-            dr [cond] += box[cond[1]]
-            dens = np.exp( - np.sum(dr*dr,axis=1) / scale )
+        scale = 2. * self.sigma**2
+        indlist = tree.query_ball_point(pos, d)
+        for n, ind in enumerate(indlist):
+            dr = gridT[ind, :] - pos[n]
+            cond = np.where(dr > box / 2.)
+            dr[cond] -= box[cond[1]]
+            cond = np.where(dr < -box / 2.)
+            dr[cond] += box[cond[1]]
+            dens = np.exp(-np.sum(dr * dr, axis=1) / scale)
             results[ind] += dens
-        
+
         return results
 
     def evaluate_pbc(self, points):
