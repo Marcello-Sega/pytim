374c88ddcd296b897d03e62cfc52033c3f7cc115
Balazs Fabian <fbalazsf@gmail.com>
2019-01-08 00:34:36 +0100

    Eliminated _define_groups()
    Also reordered the methods to reflect
    the call order / importance.

--- pytim/sanity_check.py
+++ pytim/sanity_check.py
@@ -17,6 +17,139 @@ class SanityCheck(object):
         self.interface._MDAversion = MDAnalysis.__version__
         self.interface.warnings = warnings
 
+    def assign_universe(self, universe, analysis_group, radii_dict=None):
+        """ Tweak the details of the universe:
+
+            - Compare input_obj against the possible classes. This
+              makes possible the use of an AtomGroup in place of a
+              Universe.
+            - Load the radii from file or from radii_dict
+            - Check for missing attributes
+        """
+
+        self.interface._mode = self._check_universe(universe)
+        if self.interface._mode is None:
+            raise Exception(messages.WRONG_UNIVERSE)
+
+        self.interface.all_atoms = self.interface.universe.select_atoms('all')
+
+        ag = analysis_group
+        if self.interface.analysis_group is None:
+            if ag is None:
+                _group = self.interface.all_atoms
+            elif isinstance(ag, int):
+                _group = self.interface.universe.atoms[ag:ag + 1]
+            elif isinstance(ag, list) or isinstance(ag, np.ndarray):
+                _group = self.interface.universe.atoms[ag]
+            else:
+                _group = ag
+            self.interface.analysis_group = _group
+
+        if (len(self.interface.analysis_group) == 0):
+            raise RuntimeError(messages.UNDEFINED_ANALYSIS_GROUP)
+
+        if radii_dict is not None:
+            self.interface.radii_dict = radii_dict.copy()
+        else:
+            self.interface.radii_dict = datafiles.pytim_data.vdwradii(
+                datafiles.G43A1_TOP).copy()
+            self.patch_radii_dict()
+
+        _missing_attributes(self.interface, self.interface.universe)
+
+    def _check_universe(self, input_obj):
+        """ Check whether instead of a universe,
+            the input_obj is one of the following,
+            and act accordingly:
+            - MDAnalysis.core.universe.Universe
+            - MDAnalysis.core.groups.AtomGroup
+            - mdtraj.core.trajectory.Trajectory
+            - simtk.openmm.app.simulation.Simulation
+        """
+
+        if isinstance(input_obj, MDAnalysis.core.universe.Universe):
+            self.interface.universe = input_obj
+            self.interface.analysis_group = None
+            return 'MDAnalysis'
+
+        if isinstance(input_obj, MDAnalysis.core.groups.AtomGroup):
+            self.interface.universe = input_obj.universe
+            self.interface.analysis_group = input_obj
+            return 'MDAnalysis'
+
+        try:
+            import os
+            import tempfile
+            import mdtraj
+            if isinstance(input_obj, mdtraj.core.trajectory.Trajectory):
+                _file = tempfile.NamedTemporaryFile(
+                    mode='w', suffix='.pdb', delete=False)
+                _file.close()
+                input_obj[0].save_pdb(_file.name)
+                self.interface.universe = MDAnalysis.Universe(_file.name)
+                PatchMDTRAJ(input_obj, self.interface.universe)
+                os.remove(_file.name)
+                return 'mdtraj'
+        except ImportError:
+            pass
+        try:
+            import os
+            from simtk.openmm.app.simulation import Simulation
+            from simtk.openmm.app import pdbfile
+            from simtk.unit import angstrom as openmm_AA
+
+            if isinstance(input_obj, Simulation):
+                _file = tempfile.NamedTemporaryFile(
+                    mode='w', suffix='.pdb', delete=False)
+                top = input_obj.topology
+                context = input_obj.context
+                pos = context.getState(getPositions=True).getPositions(
+                    asNumpy=True).value_in_unit(openmm_AA)
+                pdbfile.PDBFile.writeFile(
+                    topology=top, positions=pos, file=_file)
+                _file.close()
+                self.interface.universe = MDAnalysis.Universe(_file.name)
+                PatchOpenMM(input_obj, self.interface)
+                os.remove(_file.name)
+                return 'openmm'
+        except ImportError:
+            pass
+        return None
+
+    def patch_radii_dict(self):
+        """ Fix here by hand common problems with radii assignment """
+        self.interface.radii_dict['D'] = 0.0
+        self.interface.radii_dict['M'] = 0.0
+        self.interface.radii_dict['HW'] = 0.0
+        self.interface.radii_dict['Me'] = self.interface.radii_dict['CMet']
+
+    def assign_groups(self, cluster_cut, extra_cluster_groups):
+        elements = 0
+        extraelements = -1
+
+        self.interface.cluster_cut = cluster_cut
+        # we first make sure cluster_cut is either None, or an array
+        if isinstance(self.interface.cluster_cut, (int, float)):
+            self.interface.cluster_cut = np.array(
+                [float(self.interface.cluster_cut)])
+
+        self.interface.extra_cluster_groups = extra_cluster_groups
+        # same with extra_cluster_groups
+        if not isinstance(self.interface.extra_cluster_groups,
+                          (list, tuple, np.ndarray, type(None))):
+            self.interface.extra_cluster_groups = \
+                [ self.interface.extra_cluster_groups ]
+
+        interface = self.interface
+
+        if (interface.cluster_cut is not None):
+            elements = len(interface.cluster_cut)
+        if (interface.extra_cluster_groups is not None):
+            extraelements = len(interface.extra_cluster_groups)
+
+        if not (elements == 1 or elements == 1 + extraelements):
+            raise RuntimeError(messages.MISMATCH_CLUSTER_SEARCH)
+
     def assign_radii(self):
         try:
             groups = [g for g in self.interface.extra_cluster_groups[:]]
@@ -84,123 +217,6 @@ class SanityCheck(object):
                 raise ValueError(messages.WRONG_DIRECTION)
             interface.normal = dirdict[interface.directions_dict[normal]]
 
-    def _define_groups(self):
-        # we first make sure cluster_cut is either None, or an array
-        if isinstance(self.interface.cluster_cut, (int, float)):
-            self.interface.cluster_cut = np.array(
-                [float(self.interface.cluster_cut)])
-        # same with extra_cluster_groups
-        if not isinstance(self.interface.extra_cluster_groups,
-                          (list, tuple, np.ndarray, type(None))):
-            self.interface.extra_cluster_groups = \
-                [ self.interface.extra_cluster_groups ]
-
-    #TODO: rename
-    def _check_group(self, input_obj):
-        """ Check whether input_obj is one of the following,
-            and act accordingly:
-            - MDAnalysis.core.universe.Universe
-            - MDAnalysis.core.groups.AtomGroup
-            - mdtraj.core.trajectory.Trajectory
-            - simtk.openmm.app.simulation.Simulation
-        """
-
-        if isinstance(input_obj, MDAnalysis.core.universe.Universe):
-            self.interface.universe = input_obj
-            self.interface.analysis_group = None
-            return 'MDAnalysis'
-
-        if isinstance(input_obj, MDAnalysis.core.groups.AtomGroup):
-            self.interface.universe = input_obj.universe
-            self.interface.analysis_group = input_obj
-            return 'MDAnalysis'
-
-        try:
-            import os
-            import tempfile
-            import mdtraj
-            if isinstance(input_obj, mdtraj.core.trajectory.Trajectory):
-                _file = tempfile.NamedTemporaryFile(
-                    mode='w', suffix='.pdb', delete=False)
-                _file.close()
-                input_obj[0].save_pdb(_file.name)
-                self.interface.universe = MDAnalysis.Universe(_file.name)
-                PatchMDTRAJ(input_obj, self.interface.universe)
-                os.remove(_file.name)
-                return 'mdtraj'
-        except ImportError:
-            pass
-        try:
-            import os
-            from simtk.openmm.app.simulation import Simulation
-            from simtk.openmm.app import pdbfile
-            from simtk.unit import angstrom as openmm_AA
-
-            if isinstance(input_obj, Simulation):
-                _file = tempfile.NamedTemporaryFile(
-                    mode='w', suffix='.pdb', delete=False)
-                top = input_obj.topology
-                context = input_obj.context
-                pos = context.getState(getPositions=True).getPositions(
-                    asNumpy=True).value_in_unit(openmm_AA)
-                pdbfile.PDBFile.writeFile(
-                    topology=top, positions=pos, file=_file)
-                _file.close()
-                self.interface.universe = MDAnalysis.Universe(_file.name)
-                PatchOpenMM(input_obj, self.interface)
-                os.remove(_file.name)
-                return 'openmm'
-        except ImportError:
-            pass
-        return None
-
-    def assign_universe(self, universe, analysis_group, radii_dict=None):
-        """ Tweak the details of the universe:
-
-            - Compare input_obj against the possible classes. This
-              makes possible the use of an AtomGroup in place of a
-              Universe.
-            - Load the radii from file or from radii_dict
-            - Check for missing attributes
-        """
-
-        self.interface._mode = self._check_group(universe)
-        if self.interface._mode is None:
-            raise Exception(messages.WRONG_UNIVERSE)
-
-        self.interface.all_atoms = self.interface.universe.select_atoms('all')
-
-        ag = analysis_group
-        if self.interface.analysis_group is None:
-            if ag is None:
-                self.interface.analysis_group = self.interface.all_atoms
-            elif isinstance(ag, int):
-                self.interface.analysis_group = self.interface.universe.atoms[ag:ag + 1]
-            elif isinstance(ag, list) or isinstance(ag, np.ndarray):
-                self.interface.analysis_group = self.interface.universe.atoms[ag]
-            else:
-                self.interface.analysis_group = ag
-
-        if (len(self.interface.analysis_group) == 0):
-            raise RuntimeError(messages.UNDEFINED_ANALYSIS_GROUP)
-
-
-        if radii_dict is not None:
-            self.interface.radii_dict = radii_dict.copy()
-        else:
-            self.interface.radii_dict = datafiles.pytim_data.vdwradii(
-                datafiles.G43A1_TOP).copy()
-            self.patch_radii_dict()
-
-        _missing_attributes(self.interface, self.interface.universe)
-
-    def patch_radii_dict(self):
-        """ Fix here by hand common problems with radii assignment """
-        self.interface.radii_dict['D'] = 0.0
-        self.interface.radii_dict['M'] = 0.0
-        self.interface.radii_dict['HW'] = 0.0
-        self.interface.radii_dict['Me'] = self.interface.radii_dict['CMet']
-
     def assign_alpha(self, alpha):
         try:
             box = self.interface.universe.dimensions[:3]
@@ -211,26 +227,6 @@ class SanityCheck(object):
         if alpha >= np.amin(box):
             raise ValueError(messages.ALPHA_LARGE)
         self.interface.alpha = alpha
-
-    #TODO: rename
-    def assign_groups(self, cluster_cut, extra_cluster_groups):
-        elements = 0
-        extraelements = -1
-
-        self.interface.cluster_cut = cluster_cut
-
-        self.interface.extra_cluster_groups = extra_cluster_groups
-
-        self._define_groups()
-        interface = self.interface
-
-        if (interface.cluster_cut is not None):
-            elements = len(interface.cluster_cut)
-        if (interface.extra_cluster_groups is not None):
-            extraelements = len(interface.extra_cluster_groups)
-
-        if not (elements == 1 or elements == 1 + extraelements):
-            raise RuntimeError(messages.MISMATCH_CLUSTER_SEARCH)
 
     def check_multiple_layers_options(self):
         try:
