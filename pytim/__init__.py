fe528fcca8f9a26694586ace990e65b76ea7c86b
Marcello Sega <m.sega@fz-juelich.de>
2019-01-15 10:28:22 +0100

    patched numpy to fix issue with disappeared _validate_lengths

--- pytim/__init__.py
+++ pytim/__init__.py
@@ -1,6 +1,17 @@
 # -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 #from pytim.patches import PatchTrajectory, PatchOpenMM, PatchMDTRAJ
+
+import numpy
+
+try:
+    numpy.lib.arraypad._validate_lengths
+except:
+    def patch_validate_lengths(ar,crop_width):
+        return numpy.lib.arraypad._as_pairs(crop_width, ar.ndim, as_index=True)
+    numpy.lib.arraypad._validate_lengths = patch_validate_lengths
+
+
 from .simple_interface import SimpleInterface
 from .itim import ITIM
 from .gitim import GITIM
