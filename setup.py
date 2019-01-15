843927c994373f41be98b8ca2fccdea4c29b9b3b
Marcello Sega <m.sega@fz-juelich.de>
2019-01-15 11:44:54 +0100

    fixed more requirements

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    tests_require=['nose', 'coverage'],
+    tests_require=['nose>=1.3.7', 'coverage'],
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
