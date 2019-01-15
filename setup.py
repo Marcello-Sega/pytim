980e410e4e0bbb61b1565c8f318499276ca32c36
Marcello Sega <m.sega@fz-juelich.de>
2019-01-15 11:30:43 +0100

    fixed requirements

--- setup.py
+++ setup.py
@@ -125,9 +125,9 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'MDAnalysis>=0.19.1', 'PyWavelets>=0.5.2', 'numpy>=1.15.3',
+        'MDAnalysis>=0.19.2', 'PyWavelets>=0.5.2', 'numpy>=1.15.3',
         'scipy>=1.1', 'scikit-image>=0.14.1', 'cython>=0.24.1',
-        'sphinx>=1.4.3', 'matplotlib', 'pytest', 'dask>=0.20.0'
+        'sphinx>=1.4.3', 'matplotlib', 'pytest', 'dask>=1.0'
     ],
 
     # List additional groups of dependencies here (e.g. development
