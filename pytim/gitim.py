f12ea130947eab3e5c368c3481c709b15560032a
Brian Novak <brian.novak.simulation@gmail.com>
2019-03-11 18:16:41 -0500

    GITIM only working with MDAnalysis. Changed data type for self._layers in GITIM from type(universe.atoms) to self.universe.atoms[0].__class__.

--- pytim/gitim.py
+++ pytim/gitim.py
@@ -140,7 +140,7 @@ J. Chem. Phys. 138, 044110, 2013)*
         sanity.assign_alpha(alpha)
 
         self.max_layers = max_layers
-        self._layers = np.empty([max_layers], dtype=type(universe.atoms))
+        self._layers = np.empty([max_layers], dtype=self.universe.atoms[0].__class__)
         self.info = info
         self.normal = None
         self.PDB = {}
